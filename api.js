7b133ce11d8c8841c547de3b20379b003ad72f2e
DylanEngler <dylan.engler@ynov.com>
2018-04-05 17:08:28 +0200

    add

---
+++ api.js
@@ -0,0 +1,3 @@
+const program = require('commander')
+const lien = 'https://gateway.marvel.com/v1/public/'
+const code_verif = 'ts=1&apikey=1dfbefadf2a95439362a3c18bc9ef646&hash=fdc146bf317b6191b2bb9cbd6f27d2b1'
