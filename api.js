843b1733d9044d2dbc7a2231e0a9f47e9903be81
DylanEngler <dylan.engler@ynov.com>
2018-04-05 18:17:42 +0200

    push

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const program = require('commander')
+const axios = require('axios')
 const lien = 'https://gateway.marvel.com/v1/public/'
 const code_verif = 'ts=1&apikey=1dfbefadf2a95439362a3c18bc9ef646&hash=fdc146bf317b6191b2bb9cbd6f27d2b1'
+const Retour = {}
 
 program
     .version('1.0.0', '-v, --version')
@@ -22,8 +24,21 @@ program.parse(process.argv)
 
 if (program.characters) {
     const variable = 'characters'
+    axios.get(lien+variable+code_verif)
+        .then(function (response) {
+            for(let i=0; i<response.data.results;i++) {
+                retour.personnage = retour.personnage+ ' nom : '+response.data.results.i.name+' , id : ' +response.data.results.i.id
+        }
+            console.log(retour)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        ;
     } else if (program.character) {
         const variable = `characters/${program.character}`
+        
+
     } else if (program.comics) {
         const variable = 'comics'
     } else if (program.comic) {
